8d7b3e0999c2465431fffdf4aaa8d18228f9df1e
unknown <nelsonjonat@gmail.com>
2024-12-28 18:36:10 +0800

    Basic dashboard product listing view (haven't added edit or deletion yet)

--- app/manageProducts.py
+++ app/manageProducts.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, render_template, redirect, url_for, request, flash
+from werkzeug.utils import secure_filename
+from flask import Blueprint, render_template, redirect, url_for, request, flash, current_app
 from flask_login import login_required, current_user
 from .roleDecorator import role_required
+from .forms import AddProductForm
+from .models import Product
 from . import db
+import os
 
 manageProducts = Blueprint('manageProducts', __name__)
 
@@ -9,4 +13,7 @@ manageProducts = Blueprint('manageProducts', __name__)
 @login_required
 @role_required(2, 3)
 def product_listings():
-  return render_template("dashboard/manageProducts/products.html", user=current_user)
+    products = Product.query.all()
+    for product in products:
+      print(url_for('static', filename=product.image_thumbnail))
+    return render_template("dashboard/manageProducts/products.html", user=current_user, products=products)

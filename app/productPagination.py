029e3fb61604d6ecd48dc5f2c4cae7f9a0753a2f
Nelson <nelsonjonat@gmail.com>
2024-12-30 23:23:20 +0800

    product-specific page base

--- app/productPagination.py
+++ app/productPagination.py
@@ -10,3 +10,9 @@ productPagination = Blueprint('productPagination', __name__)
 def product_pagination():
   products = Product.query.all()
   return render_template("/productPagination/products.html", user=current_user, products=products)
+
+@productPagination.route('/product/<int:product_id>')
+def product_detail(product_id):
+    # Query the database for the product
+    product = Product.query.get(product_id) 
+    return render_template("/productPagination/productPage.html", user=current_user, product=product)

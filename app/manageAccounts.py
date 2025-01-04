028d9b41eeed32933415756bf41273fa5f3b8754
Benny Goh <haiguan648@gmail.com>
2025-01-04 18:41:06 +0800

    added account details page

--- app/manageAccounts.py
+++ app/manageAccounts.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask import Blueprint, render_template, redirect, url_for, request, flash, abort
 from flask_login import login_required, current_user
 from .roleDecorator import role_required
 from .models import User
@@ -46,10 +46,43 @@ def accounts_listing():
 
   return render_template("dashboard/manageAccounts/accounts.html", user=current_user, totalUsers=totalUsers, newUsers=newUsers, onlineCount=onlineCount, accounts=accounts, total_pages=total_pages, current_page=page, search_query=search_query)
 
-# @manageAccounts.route('/account-details')
-# @login_required
-# @role_required(2, 3)
-# def account_details():
+@manageAccounts.route('/account-details/<int:id>')
+@login_required
+@role_required(2, 3)
+def account_details(id):
+  selectedUser = User.query.get_or_404(id)
+
+  # admin cannot view admin and owner accounts, but owner can view any
+  if current_user.role_id == 2: 
+    if selectedUser.role_id in [2, 3]:
+      abort(404)
+
+  # admin can only view customer accounts
+  if current_user.role_id == 1:
+    abort(404)
+
+  formatted_created_at = selectedUser.created_at.strftime("%d %B %Y")
+
+  image_file = url_for('static', filename="profile_pics/" + selectedUser.image)
+
+  if selectedUser.image:
+    if selectedUser.image.startswith('http'):
+      image_file = selectedUser.image
+    else:
+      image_file = url_for('static', filename="profile_pics/" + selectedUser.image)
+  else:
+    image_file = url_for('static', filename='profile_pics/profile_image_default.jpg')
+
+  return render_template("dashboard/manageAccounts/account_details.html", user=current_user, selectedUser=selectedUser, formatted_created_at=formatted_created_at, image_file=image_file)
+
+@manageAccounts.route('/delete-account/<int:id>')
+@login_required()
+@role_required(2, 3)
+def delete_account(id):
+  selectedUser = User.query.get_or_404(id)
+
+  db.session.delete(selectedUser)
+  db.session.commit()
 
 @manageAccounts.route('/add-admin-account')
 @login_required

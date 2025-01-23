6b2afdd47e121c995c0723af6a2b8450fa03097a
Ben <haiguan648@gmail.com>
2025-01-23 13:58:39 +0800

    adjusted styles, added search and pagination for vouchers listing

--- app/manageVouchers.py
+++ app/manageVouchers.py
@@ -4,6 +4,7 @@ from .roleDecorator import role_required
 from . import db
 from .models import Voucher, VoucherType
 from .forms import VoucherForm
+from math import ceil
 
 manageVouchers = Blueprint('manageVouchers', __name__)
 
@@ -13,7 +14,25 @@ manageVouchers = Blueprint('manageVouchers', __name__)
 def vouchers_listing():
   vouchers = Voucher.query.order_by(Voucher.created_at.desc()).all()
 
-  return render_template("dashboard/manageVouchers/vouchers.html", user=current_user, vouchers=vouchers)
+  # pagination
+  page = request.args.get('page', 1, type=int)
+  per_page = 10
+
+  # search logic
+  search_query = request.args.get('q', '', type=str)
+
+  if search_query:
+    vouchers_query = Voucher.query.filter(Voucher.voucher_code.ilike(f"%{search_query}%"))
+  else:
+    vouchers_query = Voucher.query
+
+  # pagination logic
+  total_vouchers = vouchers_query.count()
+  vouchers = vouchers_query.order_by(Voucher.id).paginate(page=page, per_page=per_page)
+
+  total_pages = ceil(total_vouchers / per_page)
+
+  return render_template("dashboard/manageVouchers/vouchers.html", user=current_user, vouchers=vouchers, total_pages=total_pages, current_page=page, search_query=search_query)
 
 @manageVouchers.route('/manage-vouchers/add', methods=['GET', 'POST'])
 @login_required
